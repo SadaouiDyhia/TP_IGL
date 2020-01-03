795ddebd9e46b9d2e9d51a1680b690c76e2457d5
Djecta1005 <44314311+Djecta1005@users.noreply.github.com>
2020-01-03 16:05:19 +0100

    Update index.js

--- Serveur/ChangerGrp/db/index.js
+++ Serveur/ChangerGrp/db/index.js
@@ -114,14 +114,14 @@ const pool=mysql.createPool(
            * @param {any} resulats
            * @returns {reject}
            */
-              pool.query('SELECT Num FROM groupe WHERE NumGrp=? AND Niv="1cs" ',[Etud.grp],(err,resultas)=>
+                pool.query('SELECT Num FROM groupe WHERE NumGrp=? AND Niv="1cs" ',[Etud.grp],(err,resultas1)=>
              {
                 if(err)
               {
                   return reject(err)
               }
              
-              g1=resultas[0].Num+1
+              g1=resultas1[0].Num+1
              /** 
             * la description de la  fonction qui se charge de la requete à la base de donéé 
             * @property {Function} query
@@ -134,13 +134,12 @@ const pool=mysql.createPool(
            * @param {any} resulats
            * @returns {reject}
            */
-              pool.query('UPDATE groupe SET Num=?  WHERE NumGrp=? AND Niv= "1cs" ' ,[g1,Etud.grp],(err,resultas)=>
+               pool.query('UPDATE groupe SET Num=?  WHERE NumGrp=? AND Niv= "1cs" ' ,[g1,Etud.grp],(err,resultas2)=>
             {
                 if(err)
               {
                   return reject(err)
               }
-              return resolve(resultas[0])
               
             })
              /** 
@@ -155,13 +154,13 @@ const pool=mysql.createPool(
            * @param {any} resulats
            * @returns {reject}
            */
-            pool.query('SELECT Num FROM groupe WHERE NumGrp=? AND Niv="1cs" ',[g],(err,resultas)=>
+           pool.query('SELECT Num FROM groupe WHERE NumGrp=? AND Niv="1cs" ',[g],(err,resultas3)=>
              {
                 if(err)
               {
                   return reject(err)
               }
-              g1=resultas[0].Num-1
+              g1=resultas3[0].Num-1
                /** 
             * la description de la  fonction qui se charge de la requete à la base de donéé 
             * @property {Function} query
